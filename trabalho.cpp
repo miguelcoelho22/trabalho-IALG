373649b30552b1fe686327edf0e40c8173b56f84
MIGUEL ARTHUR DE PAULA COELHO <miguel.coelho@DCC01-20001247.ad.ufla.br>
2024-12-03 20:00:05 -0300

    salvando

---
+++ trabalho.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct series{
+
+    char Nomeserie[30];
+    int ano;
+    float nota;
+    string genero;
+    char diretor[50];
+    int ranking;
+
+};
+
+int main(){
+
+}
