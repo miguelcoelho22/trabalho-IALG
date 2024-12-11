e8c7201c1903c2db2537f37c553714e735a97c8c
miguel arthur coelho <miguelarthurmeumine@gmail.com>
2024-12-11 13:36:27 -0300

    começando o trabalho leitura de dados csv

--- trabalho.cpp
+++ trabalho.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 
 using namespace std;
 
@@ -15,5 +16,46 @@ struct series{
 };
 
 int main(){
+
+    ifstream seriesCsv("series.csv");
+
+    if(!seriesCsv){
+        cout << "erro ao abrir o arquivo" << endl;
+        return 1;
+    }
+
+    string linha;
+
+    getline(seriesCsv, linha);
+
+    int qntdSeries;
+
+    seriesCsv >> qntdSeries;
+    
+    series serie[qntdSeries];
+
+    for(int i = 0; i < qntdSeries; i++){
+        char lixo;
+        seriesCsv.getline(serie[i].Nomeserie, 30, ',');
+
+        seriesCsv >> serie[i].ano;
+
+        seriesCsv >> lixo;
+
+        seriesCsv >> serie[i].nota;
+
+        seriesCsv >> lixo;
+
+        getline(seriesCsv, serie[i].genero, ',');
+
+        
+
+        seriesCsv.getline(serie[i].diretor, 50);
+        
+
+        cout << "Nome: " << serie[i].Nomeserie << ", Ano: " << serie[i].ano 
+             << ", Nota: " << serie[i].nota << ", Gênero: " << serie[i].genero 
+             << ", Diretor: " << serie[i].diretor << endl;
+    }
 
 }
